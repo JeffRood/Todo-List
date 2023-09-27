528849b46c9ad9c54546012a4d0cceeeb9c0d63f
Jeffry Rodriguez <jerodriguez@simetricaconsulting.com>
2023-09-27 16:49:39 -0400

    fix: message invalid login

--- Todo-List-backend/routers/user.py
+++ Todo-List-backend/routers/user.py
@@ -21,11 +21,7 @@ async def login_for_access_token(email, password ):
         user = await user_service_instance.authenticate_user(email, password)
 
         if not user:
-            return HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Incorrect email or password",
-                headers={"WWW-Authenticate": "Bearer"},
-            )
+            return {"data": None, "Success": False, "message": "Incorrect email or password"}
 
         access_token_expires = timedelta(minutes=30)
         access_token = await user_service_instance.create_access_token(data={"sub": user['email'], "name": user['name'], "id": str(user['_id'])}, expires_delta=access_token_expires)
